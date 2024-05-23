591caa85d27d2d854169ea16283b1636e8887e0f
Quentin Mowat-Amiet <qmowatamiet@iese.edu>
2024-05-23 11:00:17 +0200

    feat(logging): Add custom logging module.
    - config.json: configuration file for logging to stderr and JSON file.
    - log_utils.py: contains model_log() function for logging benchmarking to .csv file for model runtimes.
    - logger.py: contains MyJSONFormatter class for parsing JSON.
    - setup.py: contains setup_logging() function to setup handlers and returns listener object.

---
+++ analysis/log/__init__.py
@@ -0,0 +1,2 @@
+from .setup import setup_logging, logger
+from .log_utils import model_log
---
+++ analysis/log/log_utils.py
@@ -0,0 +1,57 @@
+from typing import Dict, List
+from os import getenv
+from os.path import dirname, abspath, isfile
+from csv import DictWriter
+from sys import path
+
+# path.append(dirname(dirname(abspath(__file__))))
+
+from analysis.config import (
+    MODEL,
+    N_CTX,
+    N_GPU_LAYERS,
+    N_BATCH,
+)
+
+
+def model_log(
+    corpus: Dict[int, List[str]],
+    pos_attributes: List[str],
+    start_time: float,
+    end_time: float,
+) -> None:
+
+    # Calculate number of calls
+    calls = len(corpus) * len(pos_attributes)
+
+    # Define the CSV file path
+    csv_file_path = getenv("LOG_PATH", "model_log.csv")
+
+    # Define the headers for the CSV file
+    headers = ["time", "n_gpu_layers", "n_batch", "n_ctx", "calls", "model"]
+
+    # Create a list with the data to be written
+    data = [
+        {
+            "time": end_time - start_time,
+            "n_gpu_layers": N_GPU_LAYERS,
+            "n_batch": N_BATCH,
+            "n_ctx": N_CTX,
+            "calls": calls,
+            "model": MODEL,
+        }
+    ]
+
+    # Check if the file exists
+    file_exists = isfile(csv_file_path)
+
+    # Open the CSV file in append mode
+    with open(csv_file_path, "a", newline="") as file:
+        writer = DictWriter(file, fieldnames=headers)
+
+        # Write header only if the file is being created for the first time
+        if not file_exists:
+            writer.writeheader()
+
+        # Append data to the CSV file
+        writer.writerows(data)
